a12a1691b6109953838e4f524be5f4775a8bf490
mrechesketch <alexanderechevarria@Alexanders-Mac-mini.local>
2017-08-15 22:33:41 -0500

    started closure checker

---
+++ Session 7/ClosureChecker.R
@@ -0,0 +1,78 @@
+# We're going to build a "closure checker!"
+# example: "hello (my name is Alex)" is a valid string!
+# but "hey what's up (Taylor" is not!
+# similarly "())(()" would also be invalid
+
+# we can do other stuff too like an HTML checker 
+# example: "<p> this is a paragraph </p>"
+# or: "<h1> this is a header </h1>"
+
+# this is where is starts to get more complicated...
+# example: sample <- function(poop){
+#                 oh_goodness <- character(9)
+#                 if(poop){
+#                   return(FALSE)    
+#                  }
+#                  return(0)
+#            }
+
+# there we see two types of closure: () and {} and they are nested! fuck..
+
+# so how are we going to do this? Lets use a stack!
+    # a stack is like a linked list (Session 5)
+    # it can push and pop and tell you if it's empty
+# whenever we see an opener we'll push it onto the stack
+# whenever we see a closer we'll pop it off!
+# if at the end our len == 0, we're great! 
+# if at any point we try to pop from an empty stack... uh oh
+# AND in the context of the closure checker: 
+# if what we pop is unexpected or out of qorder.. big trouble!
+
+
+# I want you to implement a *stack*. The tests for your stack are already written!
+
+
+Stack_ <- setRefClass("Stack_",
+        fields = list(len = "numeric", head = "ANY"),
+        methods = list(
+
+            push = function(){
+                return(NULL)
+            },
+
+            pop = function(){
+                return(NULL)
+            },
+
+            isEmpty = function(){
+                return(TRUE)
+            }
+
+        ))
+
+Stack <- function(){
+    return(Stack_$new(len = 0, head = NULL))
+}
+
+# here's the plan of attack: we're going to loop over the string with a moving window
+# this moving window can change size according to the lengths of the openers and closers!
+# lets have a crack at some practice exercies before we try to implement it
+
+
+Checker_ <- setRefClass("Checker",
+        fields = list(patternList = "list", ourStack = "Stack_"),
+        methods = list(
+
+            checkString <- function(string){
+                return(FALSE)
+            }
+        ))
+
+Checker <- function(listOfPatterns){
+    listOfCounters <- list()
+    # see https://stackoverflow.com/questions/2436688/append-an-object-to-a-list-in-r-in-amortized-constant-time-o1
+    # on why this is ok
+    newStack <- Stack()
+    newChecker <- Checker_$new(patternList = listOfPatterns, ourStack = newStack)
+    return(newChecker)
+}
---
+++ Session 7/ClosureCheckerTests.R
@@ -0,0 +1,47 @@
+source("ClosureChecker.R")
+
+# Stack_ tests
+
+
+# constructor test
+
+stackConstructorTest <- function(){
+    newStack <- Stack()
+    stopifnot(newStack$isEmpty())
+    stopifnot( identical(newStack$head, NULL) )
+}
+
+# push test
+
+pushTest <- function(){
+    newStack <- Stack()
+    newStack$push("(")
+    newStack$push("{")
+    newStack$push("<p>")
+    stopifnot(newStack$len == 3)
+    stopifnot(newStack$head$data == "<p>")
+}
+
+# pop test 
+popTest <- function(){
+    newStack <- Stack()
+    newStack$push("(")
+    newStack$push("{")
+    newStack$push("<p>")
+    newStack$pop()
+    newStack$pop()
+    last <- newStack$pop()
+    stopifnot(last$data == "(")
+    stopifnot(newStack$isEmpty())
+    uhoh <- newStack$pop()
+    stopifnot( identical(uhoh, NULL) )
+}
+
+# run stack tests 
+
+stackTests <- function(){
+    stackConstructorTest()
+    pushTest()
+    popTest()
+    print("STACK TESTS PASSED")
+}
