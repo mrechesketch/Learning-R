ddaf3bae2b118f646f3d3e8aee8a0d4958149b90
mrechesketch <alexanderechevarria@Alexanders-Mac-mini.local>
2017-08-18 16:20:29 -0500

    more on exercises

---
+++ Session 7/exercises.R
@@ -0,0 +1,57 @@
+# practice for closure checker
+
+# hash table creation!
+# whenever we do a pop operation we need to make sure  we 
+# are getting what we expect..
+
+# given a two string-vectors, create a hashtable where 
+# the second vector elements are keys and the first vector elements are values
+
+openers <- c("(", "{", "[", "<p>", "<h1>", "<title>")
+closers <- c(")", "}", "]", "</p>", "</h1>", "</title>")
+
+# does the hashmap come with a useful constructor? maybe...
+closerDict <- hashmap()
+
+exercise_1 <- function(){
+    stopifnot( setequal(closerDict$keys(),closers) 
+    print("exercise_1 complete!")
+
+# so when we are making our moving window.. we'll need to know how big it is
+# given two vectors of openers and closers return a vectors of the lengths of the strings
+
+getLengths <- function(openers, closers){
+    # useful functions: sapply with nchar(x) [not length(x)]
+    # use union to merge sets and preserve uniqueness
+    return(c())
+}
+
+exercise_2 <- function(){
+    characterLengths <- getLengths(openers, closers)
+    stopifnot( setequal(characterLengths,c(1,2,4,7,5,8)) )
+    print("exercise_2 complete")
+}
+
+# now we are going to practice moving a window over a string
+
+sequence <- "GATCATAGCACATAGGACA" 
+stopCodon <- "TAG"
+# hint: for both of these you will want substr(string, start, stop)
+
+# how many times does the stop codon appear
+howMany <- function(sequence, pattern){
+    return(0)
+}
+
+# return the string up to the stop codon
+upToFirst <- function(sequence, pattern){
+    return("")
+}
+
+exercise_3 <- function(){
+    stopifnot(howMany(sequence, stopCodon) == 2)
+    stopifnot(upToFirst(sequence, stopCodon) == "GATCA")
+    print("exercise_3 complete")
+}
+
+
--- Session 7/movingWindowExercises.R
+++
@@ -1,1 +0,0 @@
-# practice for string manipulation 
