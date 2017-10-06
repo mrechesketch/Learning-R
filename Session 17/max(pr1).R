aa33be6bffd8ba33784d2055cbc9bacf0704fbe3
mrechesketch <alexanderechevarria@Alexanders-Mac-mini.local>
2017-10-05 22:34:17 -0500

    homework is up

---
+++ Session 17/max(pr1).R
@@ -0,0 +1,75 @@
+
+
+# part I: write your own max function
+# obviously don't use max.. 
+
+# with recursion:
+
+maxRec <- function(NumVec){
+    # base case: if there is one element, it is the max
+    # if the first is greater than the second, swap it
+    # recurse on all but the first element 
+    return( NumVec[1] ) # place holder
+}
+
+# now do it with a for loop (example done)
+
+maxFor <- function(NumVec){
+    currentMax <- NumVec[1] # currentMax is the first element 
+    for ( i in seq_along(NumVec) ){
+        if ( currentMax < NumVec[i] ) currentMax <- NumVec[i] # if you see a better one, overwrite
+    }
+    return( currentMax ) # return the current max
+}
+
+# and while loop (you might want to refresh yourself on while loops)
+# see https://www.programiz.com/r-programming/while-loop
+# hint: use while( !is.na(NumVec[i] ) ) because when you get to i = length + 1
+# that means A) you have made it through th entire vector and B) NumVec[i] will return an NA
+
+maxWhile <- function(NumVec){
+    # TODO
+    return( NumVec[1] ) # place holder
+}
+
+
+# and lastly with Reduce (can be done in one line with the ternary operator / anonymous function) NO MAX!
+# refresher on ternary operator https://stackoverflow.com/questions/8790143/does-the-ternary-operator-exist-in-r
+
+maxRed <- function(NumVec) return( NumVec[1] ) # place holder 
+
+passFail <- function(x, y) if (x == y) "PASS" else "FAIL"
+customVec <- c(3, 55, 4, 77, 32)
+
+cat("\n")
+print("maxRec Tests")
+t1 <- maxRec(1)
+t2 <- maxRec(1:10)
+t3 <- maxRec( customVec )
+cat("the output of maxRec(1) is:", t1, passFail(t1, 1), "\n" )
+cat("the output of maxRec(1:10) is:", t2, passFail(t2, 10), "\n" )
+cat("the output of maxRec( c(3, 55, 4, 77, 32) ) is:", t3, passFail(t3, 77), "\n" )
+
+print("maxFor Tests")
+t1 <- maxFor(1)
+t2 <- maxFor(1:10)
+t3 <- maxFor( customVec )
+cat("the output of maxFor(1) is:", t1, passFail(t1, 1), "\n" )
+cat("the output of maxFor(1:10) is:", t2, passFail(t2, 10), "\n" )
+cat("the output of maxFor( c(3, 55, 4, 77, 32) ) is:", t3, passFail(t3, 77), "\n" )
+
+print("maxWhile Tests")
+t1 <- maxWhile(1)
+t2 <- maxWhile(1:10)
+t3 <- maxWhile( customVec )
+cat("the output of maxWhile(1) is:", t1, passFail(t1, 1), "\n" )
+cat("the output of maxWhile(1:10) is:", t2, passFail(t2, 10), "\n" )
+cat("the output of maxWhile( c(3, 55, 4, 77, 32) ) is:", t3, passFail(t3, 77), "\n" )
+
+print("maxRed Tests")
+t1 <- maxRed(1)
+t2 <- maxRed(1:10)
+t3 <- maxRed( customVec )
+cat("the output of maxRed(1) is:", t1, passFail(t1, 1), "\n" )
+cat("the output of maxRed(1:10) is:", t2, passFail(t2, 10), "\n" )
+cat("the output of maxRed( c(3, 55, 4, 77, 32) ) is:", t3, passFail(t3, 77), "\n" )
