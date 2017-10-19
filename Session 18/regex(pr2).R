17215702948fe1a4287cfe85ed7ba8b240a8986d
octopus <octopus>
2017-10-19 11:28:14 -0500

    stuff

--- Session 18/regex(pr2).R
+++ Session 18/regex(pr2).R
@@ -21,71 +21,94 @@ hasAlpha <- function(string) grepl("[A-z]", string)
 # HINT: if you get stuck.. all of the answers are on the cheat sheet!!
 
 # this one you could just use nchar really.. 
-hasLength <- function(password) nchar(password) >= 8
+hasLength <- function(password) F
 
-hasUpper <- function(password) grepl("[A-Z]", password)
+hasUpper <- function(password) F
 
-hasLower <- function(password) grepl("[a-z]", password)
+hasLower <- function(password) F
 
-hasNum <- function(password) grepl("[0-9]", password)
+hasNum <- function(password) F
 
 # might have to resort to POSIX style here
-hasPunct <- function(password) grepl("[[:punct:]]", password)
+hasPunct <- function(password) F
 
 validPassword <- function(p){
-    hasLength(p) && hasUpper(p) && hasLower(p) && hasNum(p) && hasPunct(p)
+    hasLower(p) && hasNum(p)
 }
 
 
-# ======================= PART II: EMAILS ======================== #q
+# ======================= PART II: EMAILS ======================== #
 
 # what do emails look like anyway?
 #   ex 1: aechevarria@hmc.edu
 #   ex 2: lanalove420@gmail.com
 #   ex 3: 12skipafew100@yahoo.net
+#   ex 4: _alex.echevarria95_@gmail.com ##for the purpose of this exercise
 
-# so lets look at the common elements:
-    # all have ONE @ symbol
-    # the @ symbol is preceeded by alphanumeric with intermittent *punctuation* 
-    #   *punctuation* (only . - _ is acceptable) and you can't end on it 
-    # the @ symbol is followed by only alphabetical characters 
+# the above example emails are all valid
+
+# so lets focus on the suffix term
+    # all have at least one @ symbol 
+    # the @ symbol is followed by only lowercase alphabetical characters 
     # until the "."
     # and lastly we have some valid extensions: com, edu, net etc..
 
 # lets keep it simple and just follow the above rules
 # wikipedia has an article on email addresses: https://en.wikipedia.org/wiki/Email_address
-# they don't necessarily have to follow the same rules as above
-# but the wiki definition of email looks trick 
-
-# hence the above example emails are all valid
+# wiki rules look much trickier.. focus on the above listed rules
 
 # however 
-#   _alex.echevarria95_@gmail.com would not be because there is beginning & ending punctuation 
+#   _alex.echevarria@95_@GMAIL.com would not be because GMAIL is capiltalized 
 #   goHawks.gmail.com is missing the @ symbol
 #   no-reply@greenhouse.io does not have the correct extension
+#   aechevarria@hmc.edu@sluts.NOW contains a valid suffix but this term does not end the string hence, false 
 
-# let develop our patterns that will detect some of these things
+add1 <- "_alex.echevarria@95_@GMAIL.com"
+add2 <- "_alex.echevarria95_@gmail.com"
 
-# lets start with an easy example
+# the extract function is helpful for seeing what pattern is getting matched
+extract <- function(patt, string) regmatches(string, regexpr(patt, string, perl = T) )
+ 
+
+validEmail <- function(email){
+    # set pattern and extract string
+    emailPattern <- "\\w.+@{1}[A-z]+\\.(com|net)" # TODO, finish this pattern. it needs work!
+    extracted <- extract(emailPattern, email)
+    # set boolean values 
+    noMatch <- identical(extracted, character(0) ) # indicates no pattern found
+    partialMatch <- extracted == email # a pattern was found but does it exactly match?
+    # check empty or fullness of match
+    if ( noMatch ) F else partialMatch 
+}
+
+# ======================= PART III: USERNAMES ======================== #
+
+# for this section you get to be creative and design your own rules for a valid username
+
+# example suggestion: 
+#   1. usernames must contain a double vowel "aa" "ee" "ii" "oo" "uu"
+#   2. username can only be alphanumeric (cannot contain punctuation) 
+
+# hence "spleenSplitter69" is valid while "spleen-Splitter69!" is not ok
+
+# TODO: 
+#   1. Design a username with at least 3 rules
+#       i. at least 2 rules must have ordering (i.e. .com after @)
+#       ii. some of the rules should also be independent (like rules for password)
+#   2. Write tests for your username
+#       i. scroll to the "#=== TESTS for Username ===#" section and model the previous tests
+#   3. Write a validUsername function.
+#       i. write at least one NEW helper function
+#   also free to borrow the helper functions you wrote for validPassword
 
 
-
-# now it's your turn to try
-alphaNumPattern <- "\\w"
-isAlphaNum <- function(string) grepl(alphaNumPattern, string)
-
-alphaNumTests <- list(
-    message = "isAlphaNum",
-    inputs = c("aaaa", "a883kd", "554-!3d"),
-    expected = c(T, T, F)
-)
-
+validUsername <- function(username) T
 
 
 # Test runner function
 runTests <- function(tests){
     for ( test in tests ){
-    
+
         cat("\n")
 
         outputs <- sapply(test$inputs, test$funct)
@@ -98,6 +121,7 @@ runTests <- function(tests){
             if ( !sames[i] ){
                 # explain what happened
                 cat(" Test", i, "failed", "\n")
+                cat("  Input:", test$inputs[i], "\n")
                 cat("  Expected:", test$expected[i], "\n")
                 cat("  Recieved:", outputs[i], "\n" )
             }
@@ -149,11 +173,39 @@ passwordTests <- list(
     expected = c(F, F, F, F, F, T)
 )
 
+# ========= TESTS for Email ============= #
 
+emailTests <- list(
+    message = "validEmail",
+    funct = function(w) validEmail(w),
+    inputs = c("aechevarria@hmc.edu", "lanalove420@gmail.com", "12skipafew100@yahoo.net",
+    "_alex.echevarria95_@gmail.com", "_alex.echevarria@95_@GMAIL.com", "goHawks.gmail.com",
+    "no-reply@greenhouse.io", "aechevarria@hmc.edu@sluts.now" 
+     ),
+     expected = c(T, T, T, T, F, F, F, F)
+)
+
+# ========= TESTS for Username ============= #
+
+usernameTests <- list(
+    message = "",
+    funct = function(w) w,
+    inputs = c(),
+    expected = c()
+)
 
 
 
 # Executing the tests
-vpTests <- list(lengthTests, upperTests, lowerTests, numTests, punctTests, passwordTests)
+tests <- list(
+    vpTests = list(lengthTests, upperTests, lowerTests, numTests, punctTests, passwordTests),
+    eTests = list(emailTests),
+    uTests = list(usernameTests)
+)
 
-runTests(vpTests)
+
+for( testSuite in tests){
+    runTests(testSuite)
+    cat("\n")
+}
+
