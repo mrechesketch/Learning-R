cb95052a735548bc6113ffabe1935d631fb83b1e
Taylor Owen <taylorowen@Taylors-MacBook-Pro.local>
2017-12-01 19:57:45 -0600

    comitted

--- Session 24/PointGroup.R
+++ Session 24/PointGroup.R
@@ -1,5 +1,5 @@
 
-TABLE_SOURCE <- "../tables-csv"
+TABLE_SOURCE <- "tables-csv"
 
 
 # exercise_1:
@@ -33,11 +33,12 @@ printr <- function(){
 
 PointGroup <- function(pgName){
     # create filepath 
-    filePath <- paste0(TABLE_SOURCE, "/", pointGroup, ".csv")
+    filePath <- paste0(TABLE_SOURCE, "/", pgName, ".csv")
     # read the csv as a data frame minding the left column
     df <- read.csv(filePath, row.names = 1)
     # convert data frame to data matrix
     dm <- data.matrix(df)
+    #getting rid of unwanted Xs and weird character replacements 
     # invalid names are prefixed with X, remove the X
     colnames(dm) <- sub("X", "", colnames(dm))
     # invalid characters are converted to ., remove that as well
@@ -75,14 +76,18 @@ PointGroup <- function(pgName){
 
 extract <- function(patt, string) regmatches(string, regexpr(patt, string, perl = T) ) # this function should help
 
-leadingNumber <- function(string){
+leadingNumber <- function(string){ 
+    string <- extract("\\d+(?=\\w)", string)
+    if( identical(character(0), string)){
+        return(1)
+    }
     return( as.integer(string) )
 }
 
 
-coef <- function(pgMatrix) return()
+coef <- function(pgMatrix) sapply(colnames(pgMatrix), leadingNumber)
 
-order <- function(pgMatrix) return()
+order <- function(pgMatrix) return(sum(coef(pgMatrix)))
 
 # write another order function that just takes in the name of the point group
 # use the method outlined by Curnow 
@@ -90,8 +95,24 @@ order <- function(pgMatrix) return()
 
 
 orderFromName <- function(pgName){
-    letterValue <- c("C" = 1, "S" = 1, "D" = 12, 
+    letterValue <- c("C" = 1, "S" = 1, "D" = 2, 
                 "T" = 12, "O" = 24, "I" = 60)
+    BigLetter <- extract("C|S|D|T|O|I", pgName)
+    NumberLetter <- extract("\\d+", pgName)
+    LittleLetter <- extract("s|v|d|h|i", pgName)
+    #makes sure its working
+    #cat(BigLetter, NumberLetter, LittleLetter, "\n")
+    if( identical(NumberLetter, character(0)) ){
+        NumberLetter <- 1
+    }else{
+        NumberLetter <- as.numeric(NumberLetter)
+    }
+    if( identical(LittleLetter, character(0))){
+        LittleLetter <- 1
+    } else{
+            LittleLetter <- 2
+        }
+    return(as.vector(letterValue[BigLetter]) *NumberLetter*LittleLetter)
 }
 
 # exercise_4:
@@ -109,12 +130,24 @@ loadTable <- function(pgName){
         coefs <- coef(pointGroup)
         h <- order(pointGroup)
         # consider applying a function over the rows of the pgMatrix
+        apply(pointGroup, 1, function(row) sum(row*coefs*reducibleRep)/h)
         # https://stackoverflow.com/questions/4236368/apply-a-function-to-every-row-of-a-matrix-or-a-data-frame 
     }
 }
 
+# for C3v
+red_1 <- c(3, 0, 1)
+
+# for D3h
+red_2 <- c(5, 2, 1, 3, 0, 3)
+
+#MAGIC PROOF RESULTS
+# 2*d3h["A1'", ]+ d3h["A2''",] + d3h["E'",]
+#    E  2C3 3C2'   sh  2S3  3sv
+#    5    2    1    3    0    3
 
 # test yourself by comparing your results to NH3 and PF6 (found on the 2nd link)  
 
 # the above function only needs to return the coefficients of the irreps (A1, A2, B1, E, etc.. )
 # hence for C3v (the point group of NH3) the reduction of c(3, 0, 1) would return c(1, 0 ,1)
+
