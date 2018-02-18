a097aa37bd1682253585fecaccffcc144d32d1ba
Alex Echevarria <Alex.Echevarria@qorvo.com>
2018-02-18 16:29:02 -0600

    shit

--- Session 30/donation-analytics-master/insight_testsuite/temp/src/donation-analytics.r
+++ Session 30/donation-analytics-master/insight_testsuite/temp/src/donation-analytics.r
@@ -5,13 +5,25 @@ perc <- args[2]
 output <- args[3]
 
 
-main <- function(...){
-    args <- list(...)
-    for( item in args ) print(item)
+# main <- function(...){
+#     args <- list(...)
+#     for( item in args ) print(item)
 
+# }
+
+main <- function(input, percent, output){
+    # open the input
+    con <- file(input, "r")
+        while( TRUE ){
+        line <- readLines(con, n=1)
+        if ( length(line) == 0 ) break
+        print(line)
+    }
+    print(percent)
+    print(output)
+    
 }
 
 if( !interactive() ){
-    print("something")
     main(itcont, perc, output)
 }
--- Session 30/donation-analytics-master/src/donation-analytics.r
+++ Session 30/donation-analytics-master/src/donation-analytics.r
@@ -5,10 +5,36 @@ perc <- args[2]
 output <- args[3]
 
 
-main <- function(...){
-    args <- list(...)
-    for( item in args ) print(item)
+# main <- function(...){
+#     args <- list(...)
+#     for( item in args ) print(item)
 
+# }
+
+
+########################
+# CONST VARIABLES #
+
+CMTE_ID <- 1
+NAME <- 8
+ZIP_CODE <- 11 # only first 5
+TRANSACTION_DT <-14
+TRANSACTION_AMT <- 15 
+OTHER_ID <- 16 # needs to be empty
+
+
+
+main <- function(input, percent, output){
+    # open the input
+    con <- file(input, "r")
+        while( TRUE ){
+        line <- readLines(con, n=1)
+        if ( length(line) == 0 ) break
+        print(line)
+    }
+    print(percent)
+    print(output)
+    
 }
 
 if( !interactive() ){
